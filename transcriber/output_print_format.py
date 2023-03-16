85a81ff4d6da025c76b5d45079811538f8949833
Danilo Santitto <danilo.santitto@gmail.com>
2023-03-16 22:26:21 +0100

    refactor: output_print_format
    - add time split manager
    - extract method

--- transcriber/output_print_format.py
+++ transcriber/output_print_format.py
@@ -1,43 +1,70 @@
 import time
-
+from transcriber.title_split_manager import TitleSplitManager
 class OutputPrintFormat:
-    def __init__(self) -> None:
+    def __init__(self, timelist_file: str = None) -> None:
+        self.timelist_file = timelist_file
         pass
 
     def print_output(self, result, output_file, input_file, model_name, format=None):
+        timetitle_list = TitleSplitManager().get_title_time_list_from_file(self.timelist_file) if self.timelist_file else None
         if format is None or format == "timestamp":
-            self._print_timestamp_format(result, output_file, input_file, model_name)
+            self._print_timestamp_format(result, output_file, input_file, model_name, timetitle_list)
         if format is None or format == "plain":
-            self._print_plain_format(result, output_file, input_file, model_name)
+            self._print_plain_format(result, output_file, input_file, model_name, timetitle_list)
         elif format not in ["timestamp", "plain"]:
             raise ValueError(f"Invalid format: {format}. Supported formats: 'timestamp', 'plain'.")
 
-    def _print_timestamp_format(self, result, output_file, input_file, model_name):
+    def _print_timestamp_format(self, result, output_file, input_file, model_name, timetitle_list):
         self._write_output(
             result,
             output_file,
             input_file,
             model_name,
+            timetitle_list,
             "format timestamp",
             lambda segment: f"[{self._format_time(segment['start'])} -> {self._format_time(segment['end'])}] {segment['text']}\n"
         )
 
-    def _print_plain_format(self, result, output_file, input_file, model_name):
+    def _print_plain_format(self, result, output_file, input_file, model_name, timetitle_list):
         self._write_output(
             result,
             output_file,
             input_file,
             model_name,
+            timetitle_list,
             "format plain text",
             lambda segment: f"{segment['text']}\n"
         )
 
-    def _write_output(self, result, output_file, input_file, model_name, format_label, format_function):
+    def _write_output(self, result, output_file, input_file, model_name, timetitle_list, format_label, format_function):
         with open(output_file, "a") as outfile:
-            outfile.write(f"File: {input_file}, {model_name} model, {format_label}\n\n")
-            for segment in result["segments"]:
-                outfile.write(format_function(segment))
+            self._write_header(outfile, input_file, model_name, format_label)
+
+            if timetitle_list:
+                self._write_segments_with_titles(outfile, result, iter(timetitle_list), format_function)
+            else:
+                self._write_segments(outfile, result, format_function)
+
             outfile.write("\n\n")
+
+    def _write_header(self, outfile, input_file, model_name, format_label):
+        outfile.write(f"File: {input_file}, {model_name} model, {format_label}\n\n")
+
+    def _write_segments_with_titles(self, outfile, result, timetitle_iter, format_function):
+        timetitle = next(timetitle_iter, None)
+        for segment in result["segments"]:
+            timetitle = self._write_titles(outfile, segment, timetitle_iter, timetitle)
+            outfile.write(format_function(segment))
+
+    def _write_titles(self, outfile, segment, timetitle_iter, timetitle):
+        while timetitle and segment['start'] >= timetitle.time:
+            outfile.write(f"\n{timetitle.title}\n\n")
+            timetitle = next(timetitle_iter, None)
+        return timetitle
+
+    def _write_segments(self, outfile, result, format_function):
+        for segment in result["segments"]:
+            outfile.write(format_function(segment))
 
     def _format_time(self, timestamp):
         return time.strftime('%H:%M:%S', time.gmtime(timestamp)) + f"{timestamp % 1:.1f}"[1:]
